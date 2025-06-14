b9a6b8e48d39c0a1de2efb03c3e7fea24e624eb3
stefanoauciello <auciellostefano1@gmail.com>
2025-06-14 13:00:25 +0200

    Fix Jest config for ESM

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,8 @@
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
